7880988823d7ea0dd4fae2f453d4782ae1ba2bc8
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-04 17:07:01 +0700

    Cấu trúc lại folder PUG

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = (req, res) => { // index là tên hàm thể hiện "trang chính" của trang chủ (home)
-  res.render("client/pages/home/index"); 
+  res.render("client/pages/home/index", {
+    pageTitle: "Trang chủ"
+  }); 
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = (req, res) => { // index là tên hàm thể hiện trang chính của "trang sản phẩm"
-  res.render("client/pages/products/index");
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm"
+  });
 }
