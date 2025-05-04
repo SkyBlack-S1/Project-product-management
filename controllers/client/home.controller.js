d325e75ec051c5c1566410b239c0642e5d1db0c1
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-04 16:45:04 +0700

    Cấu hình folder controllers
    Buổi 19 YTB

---
+++ controllers/client/home.controller.js
@@ -0,0 +1,3 @@
+module.exports.index = (req, res) => { // index là tên hàm thể hiện "trang chính" của trang chủ (home)
+  res.render("client/pages/home/index"); 
+}
---
+++ controllers/client/product.controller.js
@@ -0,0 +1,3 @@
+module.exports.index = (req, res) => { // index là tên hàm thể hiện trang chính của "trang sản phẩm"
+  res.render("client/pages/products/index");
+}
