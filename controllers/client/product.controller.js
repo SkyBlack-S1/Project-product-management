83f0cfa4e857cb74298c043b6e1bf2ef7bd332b3
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-05 16:48:44 +0700

    Làm trang danh sách sản phẩm bên Client
    6.4 Slide Bai15

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,21 @@
-module.exports.index = (req, res) => { // index là tên hàm thể hiện trang chính của "trang sản phẩm"
+/* Controller : Xử lý logic, tính toán & liên kết model với view */
+const Product = require("../../models/product.model");
+module.exports.index = async (req, res) => { // index là tên hàm thể hiện trang chính của "trang sản phẩm"
+  const products = await Product.find({ // Truy vấn tất cả document ở trong collection products
+    status: "active",
+    deleted: false
+  }); 
+
+  const newProducts = products.map(item => {
+    item.priceNew = (item.price*(100 - 
+    item.discountPercentage)/100).toFixed(0);
+    return item;
+  });
+
+  console.log(newProducts);
+
   res.render("client/pages/products/index", {
-    pageTitle: "Danh sách sản phẩm"
+    pageTitle: "Danh sách sản phẩm",
+    products: newProducts
   });
 }
