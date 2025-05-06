1a56a85ec16185b0eaa72534e3d6ae9cfd23cb66
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-06 22:44:13 +0700

    Tính năng tìm kiếm ( Phía FE )
    Slide Bai16 - Phần 4
    - Mới xử lý code phía FE ( Hết buổi 20 YTB )

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -19,3 +19,23 @@ if(buttonStatus.length > 0) {
   });
 }
 /* End Button Status */
+
+/* Form Search */
+const formSearch = document.querySelector("#form-search");
+if(formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault(); // ngăn chặn sự kiện mặc định khi submit form (tránh load lại trang) | -> kết hợp với bộ lọc để tìm kiếm
+    const keyword = e.target.elements.keyword.value;
+
+    if(keyword) {
+      url.searchParams.set("keyword", keyword); // searchParams là phần sau dấu "?"
+    } else {
+      url.searchParams.delete("keyword");
+    }
+
+    window.location.href = url.href;
+  });
+}
+
+/* End Form Search */
