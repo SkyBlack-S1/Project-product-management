2a0efedb4f4c956137c485996478660f5bc2a225
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-07 21:08:48 +0700

    Tối ưu phần Phân trang
    Bai17 - 2
    Buổi 21 YTB

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filerStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
 
@@ -8,40 +10,35 @@ module.exports.index = async (req, res) => {
     deleted: false
   };
 
-  // Tính năng lọc theo trạng thái
+  /* Tính năng lọc theo trạng thái */
   const filterStatus = filterStatusHelper(req.query);
 
   if(req.query.status){
     find.status = req.query.status; // tương tự việc thêm key status vào find, với value là "active"
   }
 
-  // Tính năng tìm kiếm
+  /* Tính năng tìm kiếm */
   const objectSearch = searchHelper(req.query);
   if(objectSearch.regex) {
     find.title = objectSearch.regex;
   }
 
-  // Tính năng phân trang
-  let objectPagination = {
-    currentPage: 1,
-    limitItems: 4
-  };
-
-  if(req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
+  /* Tính năng phân trang */
   const countProducts = await Product.countDocuments(find);
-  const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
-  // console.log(totalPage);
-  objectPagination.totalPage = totalPage;
+
+  let objectPagination = paginationHelper (
+    {
+      currentPage: 1,
+      limitItems: 4
+    },
+    req.query,
+    countProducts
+  );
+  /* End Tính năng phân trang */
 
   const products = await Product.find(find)
   .limit(objectPagination.limitItems).skip(objectPagination.skip);
-  // const products = await Product.find(find);
-
+  
   res.render("admin/pages/products/index", { // hiển thị ra views
     pageTitle: "Trang sản phẩm",
     products: products,
