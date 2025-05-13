b25a0476768cd76bca597eb477db620ef7bf1a96
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-13 20:42:46 +0700

    TinyMCE
    Buổi 28 - Bai20 - 2
    - Thư viện soạn thảo văn bản tương tự như Word : https://www.tiny.cloud/docs/integrations/expressjs/
    - ...còn phần Upload ảnh từ local (TinyMCE) bổ sung sau

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config(); // nhúng dotenv
+const path = require('path');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const indexRoute = require("./routes/client/index.route");
@@ -30,6 +31,12 @@ app.set("view engine", "pug");
 app.use(cookieParser("HUYENCUTEPHOMAIQUE"));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
+
+// TinyMCE
+app.use(
+  '/tinymce', 
+  express.static(path.join(__dirname, 'node_modules', 'tinymce'))
+);
 
 // App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin; // Biến toàn cục chỉ tồn tại trong tất cả file .pug
---
+++ public/admin/js/tinymce-config.js
@@ -0,0 +1,4 @@
+tinymce.init({
+  selector: "textarea.textarea-mce", // tìm đến những thẻ textarea có class là textarea-mce để hiện trình soạn thảo
+  plugins: "image",
+});
