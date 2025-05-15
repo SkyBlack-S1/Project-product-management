16ba4d27de8910807f4f68029dd9c263213fd488
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-15 20:33:13 +0700

    Tính năng chỉnh sửa danh mục sản phẩm
    Buổi 30 - Bai21 - 1

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -10,25 +10,25 @@ module.exports.index = async (req, res) => {
 
   const records = await ProductCategory.find(find);
 
-  const newRecords = createTreeHelper.tree(records);  
+  const newRecords = createTreeHelper.tree(records);
 
   res.render("admin/pages/products-category/index", {
     pageTitle: "Trang danh mục sản phẩm",
-    records: newRecords
+    records: newRecords,
   });
 };
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
   let find = {
-    deleted: false
-  }
+    deleted: false,
+  };
 
   const records = await ProductCategory.find(find);
 
   const newRecords = createTreeHelper.tree(records);
 
-  // console.log(newRecords); // Lưu ý phải mở cửa sổ console phía BE bên trình duyệt mới xem đầy đủs
+  // console.log(newRecords); // Lưu ý phải mở cửa sổ console phía BE bên trình duyệt mới xem đầy đủ
 
   res.render("admin/pages/products-category/create", {
     pageTitle: "Tạo danh mục sản phẩm",
@@ -50,4 +50,39 @@ module.exports.createPost = async (req, res) => {
   await record.save();
 
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+    const newRecords = createTreeHelper.tree(records);
+
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+};
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne({ _id: id }, req.body);
+
+  res.redirect(req.header("Referer"));
 };
