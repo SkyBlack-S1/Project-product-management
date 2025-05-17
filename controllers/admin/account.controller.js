edd5437a830cad782c1bf2ab788ffb3ba0df0863
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-17 21:36:51 +0700

    Làm trang danh sách tài khoản & tạo tài khoản
    Buổi 32 - Bai22 - 1.2 1.3 1.4
    - Bổ sung trường "phone" cho model do phần trước quên
    - Mã hóa mật khẩu : https://www.npmjs.com/package/md5

---
+++ controllers/admin/account.controller.js
@@ -0,0 +1,60 @@
+const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
+const md5 = require("md5");
+
+// [GET] /admin/accounts
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await Account.find(find)
+    .select("-password -token"); // trả về tất cả trừ password & token
+
+  // console.log(records);
+  for (const record of records) {
+    const role = await Role.findOne({
+      _id: record.role_id,
+      deleted: false
+    });
+    record.role = role; // thêm 1 key để chứa role
+  }
+
+  res.render("admin/pages/accounts/index", {
+    pageTitle: "Danh sách tài khoản",
+    records: records,
+  });
+};
+
+// [GET] /admin/accounts/create
+module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false,
+  });
+
+  res.render("admin/pages/accounts/create", {
+    pageTitle: "Tạo mới tài khoản",
+    roles: roles,
+  });
+};
+
+// [POST] /admin/accounts/create
+module.exports.createPost = async (req, res) => {
+  const emailExist = await Account.findOne({ // Kiểm tra email đã tồn tại chưa
+    email: req.body.email,
+    deleted: false,
+  });
+
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+    res.redirect(req.header("Referer"));
+  } else {
+    req.body.password = md5(req.body.password); // mã hóa mật khẩu
+
+    const record = new Account(req.body);
+    await record.save();
+
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
+};
--- models/account.model.js
+++ models/account.model.js
@@ -10,6 +10,7 @@ const accountSchema = new mongoose.Schema(
       type: String,
       default: generateHelper.generateRandomString(20)
     }, 
+    phone: String,
     avatar: String,
     role_id: String,
     status: String,
