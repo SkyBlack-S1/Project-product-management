217edb77771d6749a59f0f512b5b4903bc727b52
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-23 22:34:06 +0700

    Hiển thị danh sách sản phẩm mới nhất
    Buổi 35 - Bai24 - 4.2 - Time 41:00
    Làm trang chủ bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,18 +3,26 @@ const productsHelper = require("../../helpers/products");
 
 // [GET] /
 module.exports.index = async (req, res) => { // index là tên hàm thể hiện "trang chính" của trang chủ (home)
-  // Lấy ra sp nổi bật
+  // Lấy ra DS sp nổi bật
   const productsFeatured = await Product.find({
     deleted: false,
     featured: "1",
     status: "active"
   }).limit(6);
-  const newProducts = productsHelper.priceNewProduct(productsFeatured);
-  // End Lấy ra sp nổi bật
-  
+  const newProductsFeatured = productsHelper.priceNewProduct(productsFeatured);
+  // End Lấy ra DS sp nổi bật
+
+  // Lấy ra DS sp mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active"
+  }).sort({position: "desc"}).limit(6);
+  const newProductsNew = productsHelper.priceNewProduct(productsNew);
+  // End Lấy ra DS sp mới nhất
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew
   }); 
 }
