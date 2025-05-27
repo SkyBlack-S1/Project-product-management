349e457051bc6d5d0cf22a72ac9d442dd566e643
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-27 22:05:28 +0700

    Xóa sản phẩm trong giỏ hàng
    Buổi 37 - Bai25 - 2.4 - Time 39:00
    - Do tính năng xóa sp ở giỏ hàng không quá quan trọng nên dùng phương thức GET (User nếu biết đường dẫn vẫn xóa được bình thường)
    - Cách xóa Object trong mảng : https://stackoverflow.com/questions/40588709/how-to-remove-object-from-array-using-mongoose

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -27,7 +27,7 @@ module.exports.index = async (req, res) => {
 
   cart.totalPrices = cart.products.reduce((sum, item) => (sum + item.totalPrice), 0);
 
-  console.log(cart);
+  // console.log(cart);
 
   res.render("client/pages/cart/index", {
     pageTitle: "Giỏ hàng",
@@ -85,3 +85,21 @@ module.exports.addPost = async (req, res) => {
   res.redirect(req.header("Referer"));
 };
 //-------------- End Thêm sản phẩm vào giỏ hàng --------------//
+
+
+//------------- Xóa sản phẩm trong giỏ hàng -------------//
+// [GET] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  
+  await Cart.updateOne({
+    _id:cartId
+  }, {
+    $pull: { products: { product_id: productId }}
+  });
+
+  req.flash("success", "Đã xóa sản phẩm khỏi giỏ hàng!");
+  res.redirect(req.header("Referer"));
+}
+//------------- End Xóa sản phẩm trong giỏ hàng -------------//
