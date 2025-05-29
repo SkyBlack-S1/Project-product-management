a37c997644f62cdc409f327e0ba5f547c5b720c7
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-29 20:14:18 +0700

    Đăng nhập
    Buổi 38 - Bai26 - 2.3 - Time 1:06:00

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,14 +3,16 @@ const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
-
 const userRoutes = require("./user.route");
+
 const categoryMiddleware = require("../../middlewares/clients/category.middleware");
 const cartMiddleware = require("../../middlewares/clients/cart.middleware");
+const userMiddleware = require("../../middlewares/clients/user.middleware");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
+  app.use(userMiddleware.infoUser);
 
   app.use("/", homeRoutes);
   
