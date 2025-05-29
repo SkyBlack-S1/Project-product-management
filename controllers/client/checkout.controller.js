6c47181f0d368530d31b1ae8040be059a4a70a99
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-05-29 09:36:57 +0700

    Làm trang đặt hàng thành công
    Buổi 38 - Bai26 - 1.4 P2

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -90,9 +90,27 @@ module.exports.order = async (req, res) => {
 //---------- Trang thông báo khi đặt hàng thành công ----------//
 // [GET] /checkout/success/:orderId
 module.exports.success = async (req, res) => {
-  console.log(req.params.orderId);
-  
+  const order = await Order.findOne({
+    _id: req.params.orderId
+  })
+
+  for (const product of order.products) {
+    const productInfo = await Product.findOne({
+      _id: product.product_id
+    }).select("title thumbnail");
+    
+    product.productInfo = productInfo;
+    product.priceNew = productsHelper.priceNewProduct(product);
+    product.totalPrice = product.priceNew * product.quantity;
+  }
+
+  order.totalPrices = order.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+
   res.render("client/pages/checkout/success", {
     pageTitle: "Đặt hàng thành công",
+    order: order,
   });
 };
