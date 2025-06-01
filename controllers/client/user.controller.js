8f7c085dde02de855178469bc20fd20393c59edf
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-06-01 20:52:58 +0700

    Tối ưu giỏ hàng
    Buổi 39 - Bai27 - 2
    Update lại code như yêu cầu của commit trước đó (Lưu thêm user_id vào collection carts)

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -72,14 +72,19 @@ module.exports.loginPost = async (req, res) => {
     return;
   }
 
-  // console.log(req.cookies.cartId);
-  // console.log(user.id);
-
-  await Cart.updateOne({
-    _id: req.cookies.cartId
-  }, {
+  const cart = await Cart.findOne({
     user_id: user.id
   });
+
+  if(cart) {
+    res.cookie("cartId", cart.id);
+  } else {
+    await Cart.updateOne({
+      _id: req.cookies.cartId
+    }, {
+      user_id: user.id
+    });
+  }
 
   res.cookie("tokenUser", user.tokenUser);
 
@@ -90,6 +95,7 @@ module.exports.loginPost = async (req, res) => {
 // [GET] /user/logout
 module.exports.logout = async (req, res) => {
   res.clearCookie("tokenUser");
+  res.clearCookie("cartId");
   res.redirect("/");
 };
 
